3a08157028632ccba758120b3ea7641aa279d515
Phil Howard <phil@gadgetoid.com>
2023-08-21 12:03:42 +0100

    PicoVector: Add vector graphics library.

--- firmware/PIMORONI_PICOVISION/micropython.cmake
+++ firmware/PIMORONI_PICOVISION/micropython.cmake
@@ -18,6 +18,7 @@ include(pimoroni_bus/micropython)
 # Pico Graphics Essential
 include(hershey_fonts/micropython)
 include(bitmap_fonts/micropython)
+include(picovector/micropython)
 include(modules/picographics/micropython)
 
 # Pico Graphics Extra
