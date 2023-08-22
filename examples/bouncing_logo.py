6f4cdd7ccb97b258b13d8f037edf6ecc001f8183
Irvin <makosa.irvin@gmail.com>
2023-08-22 17:44:03 +0100

    Added gradient background

--- examples/bouncing_logo.py
+++ examples/bouncing_logo.py
@@ -16,6 +16,9 @@ display = PicoGraphics(DISPLAY_PICOVISION, width=640, height=480, pen_type=PEN)
 WIDTH, HEIGHT = display.get_bounds()
 BLACK = display.create_pen(0, 0, 0)
 WHITE = display.create_pen(255, 255, 255)
+NAME = "PIMORONI"
+DROP_SHADOW_OFFSET = 5
+TEXT_SCALE = 10
 
 # image indices
 LOGOSUB1_INDEX = 1
@@ -23,10 +26,6 @@ LOGOSUB2_INDEX = 2
 LOGOSUB3_INDEX = 3
 LOGOSUB4_INDEX = 4
 
-SPLATSUB1_INDEX = 5
-SPLATSUB2_INDEX = 6
-SPLATSUB3_INDEX = 7
-SPLATSUB4_INDEX = 8
 
 MAX_SPRITE_SLOTS = 16 
 MAX_LOGOS = MAX_SPRITE_SLOTS //4
@@ -44,16 +43,7 @@ try:
         display.load_sprite("pim2.png", LOGOSUB2_INDEX)
         display.load_sprite("pim3.png", LOGOSUB3_INDEX)
         display.load_sprite("pim4.png", LOGOSUB4_INDEX)
-        
-        # load explosion
-        display.load_sprite("fire2-0-0.png", SPLATSUB1_INDEX)
-        display.load_sprite("fire2-1-0.png", SPLATSUB2_INDEX)
-        display.load_sprite("fire2-0-1.png", SPLATSUB3_INDEX)
-        display.load_sprite("fire2-1-1.png", SPLATSUB4_INDEX)
-        display.update()
-        
-        
-
+    
     has_sprite = True
 except OSError as ioe:
     if ioe.errno not in (errno.ENOENT):
@@ -66,7 +56,7 @@ logos_count = 0
 # specify image dimensions and randomise starting position
 IMAGE_WIDTH = 64
 IMAGE_HEIGHT = 64
-DEFAULT_VELOCITY = 15  # set the default velocity of the image
+DEFAULT_VELOCITY = 20  # set the default velocity of the image
 
 
 class Logo:
@@ -87,7 +77,7 @@ class Logo:
         if has_sprite:
             display.display_sprite(((sprite_slot *4) -3),LOGOSUB1_INDEX,self.x_start, self.y_start)
             display.display_sprite((sprite_slot *4) -2,LOGOSUB2_INDEX,self.x_start + 32, self.y_start)
-            display.display_sprite((sprite_slot *4) -1,LOGOSUB3_INDEX,self.x_start, self.y_start + 32)
+#             display.display_sprite((sprite_slot *4) -1,LOGOSUB3_INDEX,self.x_start, self.y_start + 32)
             display.display_sprite(sprite_slot*4,LOGOSUB4_INDEX,self.x_start + 32, self.y_start + 32)
         else:
             display.rectangle(self.x_start, self.y_start, 100, 100)
@@ -101,15 +91,6 @@ class Logo:
 
 
 
-# generate random colour when called
-def change_pen_colour():
-    r = random.randint(20, 255)
-    g = random.randint(20, 255)
-    b = random.randint(20, 255)
-    text_colour = display.create_pen(r, g, b)
-    return text_colour
-
-
 # check for edge collisions and adjust velocities and positions accordingly
 def edge_collision(logo):
     global text_colour
@@ -119,7 +100,7 @@ def edge_collision(logo):
             logo.x_start = 0
         elif (logo.x_start + IMAGE_WIDTH) > WIDTH:
             logo.x_start = WIDTH - IMAGE_WIDTH
-        text_colour = change_pen_colour()
+        
 
     if (logo.y_start + IMAGE_HEIGHT) >= HEIGHT or logo.y_start <= 0:
         logo.y_vel = -logo.y_vel
@@ -127,7 +108,7 @@ def edge_collision(logo):
             logo.y_start = 0
         elif (logo.y_start + IMAGE_HEIGHT) > HEIGHT:
             logo.y_start = HEIGHT - IMAGE_HEIGHT
-        text_colour = change_pen_colour()
+        
 
 
 # handle logo collisions
@@ -136,7 +117,6 @@ y_offset = DEFAULT_VELOCITY // 2
 
 
 def object_collision(j):
-    splattered = False
     for i in range(logos_count):
         if j != i:  # ensures distinct logos
             # right collision
@@ -148,8 +128,7 @@ def object_collision(j):
                     logos[i].x_vel *= -1
                 if logos[j].x_vel > 0:
                     logos[j].x_vel *= -1
-                add_splatter(logos[j].x_start, logos[j].y_start)
-                splattered = True
+                
 
             # left collision
             elif (
@@ -160,8 +139,7 @@ def object_collision(j):
                     logos[i].x_vel *= -1
                 if logos[j].x_vel < 0:
                     logos[j].x_vel *= -1
-                add_splatter(logos[j].x_start, logos[j].y_start)
-                splattered = True
+                
 
             # up collision
             elif (
@@ -172,8 +150,7 @@ def object_collision(j):
                     logos[i].y_vel *= -1
                 if logos[j].y_vel > 0:
                     logos[j].y_vel *= -1
-                add_splatter(logos[j].x_start, logos[j].y_start)
-                splattered = True
+                
 
             # down collision
             elif (
@@ -184,12 +161,8 @@ def object_collision(j):
                     logos[i].y_vel *= -1
                 if logos[j].y_vel < 0:
                     logos[j].y_vel *= -1
-                add_splatter(logos[j].x_start, logos[j].y_start)
-                splattered = True
                 
-    if not splattered:
-        clear_splatter()
-
+                
 
 # add logos
 def add_logo():
@@ -198,53 +171,20 @@ def add_logo():
     logos.append(new_logo)
 
     logos_count += 1
-def add_splatter(x, y):
-    display.display_sprite(17,SPLATSUB1_INDEX,x, y)
-    display.display_sprite(18,SPLATSUB2_INDEX,x + 32, y)
-    display.display_sprite(19,SPLATSUB3_INDEX,x, y + 32)
-    display.display_sprite(20,SPLATSUB4_INDEX,x + 32, y + 32)
 
-def clear_splatter():
-    x = 800
-    y = 600
-    display.display_sprite(17,SPLATSUB1_INDEX,x, y)
-    display.display_sprite(18,SPLATSUB2_INDEX,x + 32, y)
-    display.display_sprite(19,SPLATSUB3_INDEX,x, y + 32)
-    display.display_sprite(20,SPLATSUB4_INDEX,x + 32, y + 32)
-
-def hsv_to_rgb(h, s, v):
-    if s == 0.0:
-        return v, v, v
-    i = int(h * 6.0)
-    f = (h * 6.0) - i
-    p = v * (1.0 - s)
-    q = v * (1.0 - s * f)
-    t = v * (1.0 - s * (1.0 - f))
-    i = i % 6
-    if i == 0:
-        return v, t, p
-    if i == 1:
-        return q, v, p
-    if i == 2:
-        return p, v, t
-    if i == 3:
-        return p, q, v
-    if i == 4:
-        return t, p, v
-    if i == 5:
-        return v, p, q
-
-
-hue = 0
 def draw_background():
-    global hue
-    hue += 1
-    r, g, b = [int(255 * c) for c in hsv_to_rgb(hue / 360.0, 1.0, 1.0)]  # rainbow magic
-    RAINBOW = display.create_pen(r, g, b)  # Create pen with converted HSV value
-    display.set_pen(RAINBOW)  # Set pen
-    display.clear()
+    """Draws a gradient background """
+    t = time.ticks_ms() / 1000.0
+    grid_size = 80
+    for y in range(0, HEIGHT// grid_size):
+        for x in range(0, WIDTH // grid_size):
+            h = x + y + int(t * 5)
+            h = h / 50.0
+            
 
-    
+            display.set_pen(display.create_pen_hsv(h, 0.5, 1))
+            display.rectangle(x * grid_size, y * grid_size, grid_size, grid_size)
+
     
 text_colour = WHITE
 
@@ -263,15 +203,19 @@ while True:
             print(logos_count)
         last_time = current_time
 
-    # fills the screen with black
-    draw_background()
-        
     
-    display.set_pen(text_colour)
-    text_width = display.measure_text("PIMORONI", scale=10)
-    display.text("PIMORONI", ((WIDTH - text_width) // 2), HEIGHT // 2, scale=10)
+    draw_background()
+    
+    # display name with offset
+    text_width = display.measure_text(NAME, scale=TEXT_SCALE)
+    text_height = int(7.5 * TEXT_SCALE)
+    display.set_pen(BLACK)
+    display.text(NAME, ((WIDTH - text_width) // 2) - DROP_SHADOW_OFFSET, (HEIGHT // 2 - text_height//2) + DROP_SHADOW_OFFSET, scale = 10)
 
-    # update positions for each logo and account for collisions
+    display.set_pen(WHITE)
+    display.text(NAME, ((WIDTH - text_width) // 2), HEIGHT // 2 - text_height//2, scale=10)
+
+    # update positions for each logo and account for edge and object collisions
     for num in range(logos_count):
         logo = logos[num]
         logo.x_start += logo.x_vel
