a6a579f39e5b2a69bd6c3bfab646a3b87a1c1f9f
Mike Bell <mdb036@gmail.com>
2023-08-29 19:45:55 +0100

    5 bits for v scale

--- drivers/dv_display/dv_display.cpp
+++ drivers/dv_display/dv_display.cpp
@@ -472,7 +472,7 @@ namespace pimoroni {
   void DVDisplay::set_sprite(int sprite_num, uint16_t sprite_data_idx, const Point &p, SpriteBlendMode blend_mode, int v_scale)
   {
     uint8_t buf[7];
-    buf[0] = (uint8_t)blend_mode | ((v_scale - 1) << 4);
+    buf[0] = (uint8_t)blend_mode | ((v_scale - 1) << 3);
     buf[1] = sprite_data_idx & 0xff;
     buf[2] = sprite_data_idx >> 8;
     buf[3] = p.x & 0xff;
