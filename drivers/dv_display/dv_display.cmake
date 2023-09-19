2df738dcfe46fe8c1b957861f383ea8aa3384e8e
Charlie Birks <charlie@daft.games>
2023-09-19 15:34:17 +0100

    Split swd_load into a seperate helper lib
    For slightly more advanced usage

--- drivers/dv_display/dv_display.cmake
+++ drivers/dv_display/dv_display.cmake
@@ -1,13 +1,24 @@
 set(DRIVER_NAME dv_display)
+
+# SWD loader
+add_library(swd_load INTERFACE)
+
+target_sources(swd_load INTERFACE
+  ${CMAKE_CURRENT_LIST_DIR}/swd_load.cpp)
+
+target_include_directories(swd_load INTERFACE ${CMAKE_CURRENT_LIST_DIR})
+
+pico_generate_pio_header(swd_load ${CMAKE_CURRENT_LIST_DIR}/swd.pio)
+
+target_link_libraries(swd_load INTERFACE pico_stdlib)
+
+# main DV display driver
 add_library(${DRIVER_NAME} INTERFACE)
 
 target_sources(${DRIVER_NAME} INTERFACE
-  ${CMAKE_CURRENT_LIST_DIR}/${DRIVER_NAME}.cpp
-  ${CMAKE_CURRENT_LIST_DIR}/swd_load.cpp)
+  ${CMAKE_CURRENT_LIST_DIR}/${DRIVER_NAME}.cpp)
 
 target_include_directories(${DRIVER_NAME} INTERFACE ${CMAKE_CURRENT_LIST_DIR})
 
-pico_generate_pio_header(${DRIVER_NAME} ${CMAKE_CURRENT_LIST_DIR}/swd.pio)
-
 # Pull in pico libraries that we need
-target_link_libraries(${DRIVER_NAME} INTERFACE pico_stdlib aps6404 pimoroni_i2c)
+target_link_libraries(${DRIVER_NAME} INTERFACE swd_load pico_stdlib aps6404 pimoroni_i2c)
